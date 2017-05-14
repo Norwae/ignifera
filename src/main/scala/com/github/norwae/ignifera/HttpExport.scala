5e9d5671d11de87d79d9cd715e7cb075c4f6aa3e
Terra <terra@spacetime.de>
2017-05-14 22:06:26 +0200

    Fixed access permissions

--- src/main/scala/com/github/norwae/ignifera/HttpExport.scala
+++ src/main/scala/com/github/norwae/ignifera/HttpExport.scala
@@ -19,7 +19,7 @@ import io.prometheus.client.hotspot.DefaultExports
 trait HttpExport {
   DefaultExports.initialize()
 
-  protected val registry: CollectorRegistry = CollectorRegistry.defaultRegistry
+  val registry: CollectorRegistry = CollectorRegistry.defaultRegistry
 
   /**
     * Queries the [[registry]] and produces an [[HttpResponse]] object that encapsulates
