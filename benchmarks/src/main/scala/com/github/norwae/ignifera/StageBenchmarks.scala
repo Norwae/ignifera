1ddf34d60505ce87798423ae71a258094552cb9e
Norwae <schulz.stefan@gmail.com>
2018-08-21 15:40:05 +0200

    Added benchmarks for combined and "loaded" stages

--- benchmarks/src/main/scala/com/github/norwae/ignifera/StageBenchmarks.scala
+++ benchmarks/src/main/scala/com/github/norwae/ignifera/StageBenchmarks.scala
@@ -1,10 +1,24 @@
 package com.github.norwae.ignifera
 
+import com.typesafe.config.ConfigFactory
 import org.openjdk.jmh.annotations._
+import org.slf4j.LoggerFactory
 
 class BaselineHarness extends Harness(identity)
-class StatsHarness extends Harness(StatsCollector.apply(_))
+class StatsHarness extends Harness(StatsCollector.apply(_, Nil : _*))
+class StatsBothListenersHarness extends Harness(StatsCollector.apply(_,
+  new PrometheusStatisticsListener(new HttpCollectors(ConfigFactory.empty())),
+  new AccessLogWriterListener(LoggerFactory.getLogger(classOf[StageBenchmarks]))
+))
 class GSSHarness extends Harness(GracefulShutdownSupport.apply(_))
+class FullHarness extends Harness(flow ⇒
+  GracefulShutdownSupport(
+    StatsCollector.apply(flow,
+      new PrometheusStatisticsListener(new HttpCollectors(ConfigFactory.empty())),
+      new AccessLogWriterListener(LoggerFactory.getLogger(classOf[StageBenchmarks]))
+    )
+  )
+)
 
 class StageBenchmarks {
   @Benchmark @BenchmarkMode(Array(Mode.Throughput))
@@ -20,7 +34,19 @@ class StageBenchmarks {
   }
 
   @Benchmark @BenchmarkMode(Array(Mode.Throughput))
+  def statsWithBothListeners(harness: StatsBothListenersHarness): Unit = {
+    import harness._
+    runRequest()
+  }
+
+  @Benchmark @BenchmarkMode(Array(Mode.Throughput))
   def gss(harness: GSSHarness): Unit = {
+    import harness._
+    runRequest()
+  }
+
+  @Benchmark @BenchmarkMode(Array(Mode.Throughput))
+  def full(harness: FullHarness): Unit = {
     import harness._
     runRequest()
   }
